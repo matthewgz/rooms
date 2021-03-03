25e54f2793ef6436583fed6ac12b384a6ea20402
Matthew Rosell Felix <matthewgz241@gmail.com>
2021-03-03 01:30:04 -0500

    deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(__dirname, '/dist/*'));
+app.use(express.static(`${__dirname}/dist/*`));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname, '/dist/index.html');
+  res.sendFile(__dirname, +'/dist/index.html');
 });
 
 app.listen(port);
